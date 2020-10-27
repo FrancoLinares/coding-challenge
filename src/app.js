96eedbb6b66dd34199500d8b91a6194fd764db19
FrancoLinares <franco.linares@gmail.com>
2020-10-27 11:51:02 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+	return <h1>HelloW</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
